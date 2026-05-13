4f50895eb3501db7c6be92673c4798a79e567f67
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-05-13 23:10:34 +0530

    initialize App component with Profile import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import './Profile.jsx'
+import Profile from './Profile'
 
 function App() {
   return (
