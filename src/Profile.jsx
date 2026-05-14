df712c86dcdd540bd527f5db33a1a4190b9660ac
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-05-14 22:53:31 +0530

    add Profile component with custom Avatar and Card styling

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -19,14 +19,49 @@ function Avatar({ person, size }) {
     );
 }
 
+function Card({ children, title }) {
+    return (
+        <div className="card">
+            <div className="card-content">
+                <h1>{title}</h1>
+                {children}
+            </div>
+        </div>
+    );
+}
+
 export default function Profile() {
     return (
-        <Avatar
-            size={40}
-            person={{
-                name: 'Gregorio Y. Zara',
-                imageId: '7vQD0fP'
-            }}
-        />
+        <>
+            <Avatar
+                size={120}
+                person={{
+                    name: 'Gregorio Y. Zara',
+                    imageId: '7vQD0fP'
+                }}
+            />
+            <Avatar
+                size={180}
+                person={{
+                    name: 'Gregorio Y. Zara',
+                    imageId: '7vQD0fP'
+                }}
+            />
+
+            <div>
+                <Card title="Photo">
+                    <img
+                        className="avatar"
+                        src="https://react.dev/images/docs/scientists/OKS67lhm.jpg"
+                        alt="Aklilu Lemma"
+                        width={100}
+                        height={100}
+                    />
+                </Card>
+                <Card title="About">
+                    <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
+                </Card>
+            </div>
+        </>
     );
 }
