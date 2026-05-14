2c1905695c3bec5062d6ac7d9232582fd0603109
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-05-15 02:50:18 +0530

    To simplify it, extract a Recipe component from it which will accept id, name, and ingredients props.

--- src/RecipeList.jsx
+++ src/RecipeList.jsx
@@ -3,20 +3,23 @@ import { recipes } from "./recipe_data.js";
 // Make a list of recipes from this array! For each recipe in the array, display its name as an <h2> and list its ingredients in a <ul>.
 
 function Recipe({ id, name, ingredients }) {
-    return
-    <div key={id}>
-        <h2>{name}</h2>
-        <ul>
-            {ingredients.map((ingredient) => <li key={ingredient}>{ingredient} </li>)}
-        </ul>
-    </div>
+    return (
+        <div>
+            <h2>{name}</h2>
+            <ul>
+                {ingredients.map((ingredient) => <li key={ingredient}>{ingredient} </li>)}
+            </ul>
+        </div>
+    )
 }
 
 
 export default function RecipeList() {
     return (
         <div>
-
+            <h1>Recipes</h1>
+            {recipes.map(recipe => <Recipe {...recipe} key={recipe.id} />)}
+            {/* {recipes.map((recipe) => <Recipe id={recipe.id} name={recipe.name} ingredients={recipe.ingredients} key={recipe.id} />)} */}
         </div>
     );
 }
