dff03d1ceef1fd74b396aa42768907599f601f12
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-05-16 23:34:56 +0530

    replaced firstName and lastName with state variables declared by calling useState

--- src/Forms.jsx
+++ src/Forms.jsx
@@ -3,18 +3,16 @@ import { useState } from "react";
 export default function Form() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  
+
   function handleFirstNameChange(e) {
-    firstName = e.target.value;
+    setFirstName(e.target.value);
   }
 
   function handleLastNameChange(e) {
-    lastName = e.target.value;
+    setLastName(e.target.value);
   }
 
   function handleReset() {
-    firstName = "";
-    lastName = "";
   }
 
   return (
