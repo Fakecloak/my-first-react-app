38274043596d07d9d5f6f984492e8182899a6dc8
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-05-17 02:47:18 +0530

    add states for every color click

--- src/bg_changing.jsx
+++ src/bg_changing.jsx
@@ -5,29 +5,68 @@ const COLORS = ["pink", "green", "blue", "yellow", "purple"];
 export default function BgChanger() { 
     const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
     const [clickCount, setClickCount] = useState(0);
-  const onButtonClick = (color) => () => {
-    setClickCount((clickCount) => clickCount + 1);
-    setBackgroundColor(color);
-  };
+    const [sameColorClick, setSameColorClick] = useState(0);
 
-  return (
-    <div
-      className="App"
-      style={{
-        backgroundColor,
-      }}
-    >
-        <h1> Total clicks: {clickCount}</h1>
-      {COLORS.map((color) => (
-        <button
-          type="button"
-          key={color}
-          onClick={onButtonClick(color)}
-          className={backgroundColor === color ? "selected" : ""}
+    const [pinkClickCount, setPinkClickCount] = useState(0);
+    const [greenClickCount, setGreenClickCount] = useState(0);
+    const [blueClickCount, setBlueClickCount] = useState(0);
+    const [yellowClickCount, setYellowClickCount] = useState(0);
+    const [purpleClickCount, setPurpleClickCount] = useState(0);
+
+    const onButtonClick = (color) => () => {
+        setClickCount((clickCount) => clickCount + 1);
+
+        if (backgroundColor === color){
+            setSameColorClick((sameColorClick) => sameColorClick + 1);
+        }
+
+        if (backgroundColor === "pink"){
+            setPinkClickCount((pinkClickCount) => pinkClickCount + 1);
+        }
+
+        if (backgroundColor === "green"){
+            setGreenClickCount((greenClickCount) => greenClickCount + 1);
+        }
+
+        if (backgroundColor === "blue"){
+            setBlueClickCount((blueClickCount) => blueClickCount + 1);
+        }
+
+        if (backgroundColor === "yellow"){
+            setYellowClickCount((yellowClickCount) => yellowClickCount + 1);
+        }
+
+        if (backgroundColor === "purple"){
+            setPurpleClickCount((purpleClickCount) => purpleClickCount + 1);
+        }
+
+        setBackgroundColor(color);
+    };
+
+    return (
+        <div
+        className="App"
+        style={{
+            backgroundColor,
+        }}
         >
-          {color}
-        </button>
-      ))}
-    </div>
-  );
+            <h1> Total clicks: {clickCount}</h1>
+            <h2> Same color clicked: {sameColorClick}</h2>
+            <h4> pink clicks: {pinkClickCount}</h4>
+            <h4> green clicks: {greenClickCount}</h4>
+            <h4> blue clicks: {blueClickCount}</h4>
+            <h4> yellow clicks: {yellowClickCount}</h4>
+            <h4> purple clicks: {purpleClickCount}</h4>
+        {COLORS.map((color) => (
+            <button
+            type="button"
+            key={color}
+            onClick={onButtonClick(color)}
+            className={backgroundColor === color ? "selected" : ""}
+            >
+            {color}
+            </button>
+        ))}
+        </div>
+    );
 }
