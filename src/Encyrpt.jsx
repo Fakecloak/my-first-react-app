fc21e72b7187da39ff3dc1a7050240b37921504c
Hari Prasad <128630377+Fakecloak@users.noreply.github.com>
2026-05-26 01:59:44 +0530

    update useEffect dependency array in ChatRoom component

--- src/Encyrpt.jsx
+++ src/Encyrpt.jsx
@@ -31,8 +31,7 @@ function ChatRoom({ roomId, createConnection }) {
     const connection = createConnection(roomId);
     connection.connect();
     return () => connection.disconnect();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [roomId]);
+  }, [roomId, createConnection]);
 
   return <h1>Welcome to the {roomId} room!</h1>;
 }
